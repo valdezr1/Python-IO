766dcc020d42ce3572b5273b55aebb06031335d5
Rafael Valdez <valdezr1@outlook.com>
2017-10-09 10:43:41 -0700

    Added an array of set responses

--- Main.py
+++ Main.py
@@ -8,21 +8,27 @@ userName = input()
 print("Wonderful to meet you, " + userName + ".")
 print("Now, time to get you better at Addition.")
 
+correctArr = [userName + ", are you sure you aren't an addition wizz?" , "Booyah!", "You're a human calculator!",
+              "Giving ya a Virtual Thumbs-Up!", "Add 'em, Cowboy!", "Bow-chika-wow-wow", "Awwww Yeahhhhh", "Sweetness!"]
+incorrectArr = ["YOU SHALL NOT PASS! Try again...", "So close...yet so far", "Not quite", "Better luck next try",
+                "Nice try...", "Try again, " + userName, "Even the best get it wrong sometimes", "Missed it!"]
+
 while True:
-    first = random.randint(0,1000)
-    second = random.randint(0,1000)
+    first = random.randint(0, 1000)
+    second = random.randint(0, 1000)
 
     firstStr = str(first)
     secondStr = str(second)
 
     print("Question: " + firstStr + " + " + secondStr + " =  ?")
-    print("Answer:")
+    print("Your Answer:")
 
     answer = input()
 
     if answer == str(first + second):
-        print("BABAM!!! NICE WORK!")
+        print(correctArr[random.randint(0, 7)])
     else:
-        print("Not quite")
+        print("Correct Answer: " + str(first + second))
+        print(incorrectArr[random.randint(0, 7)])
 
     print("Onto the next Question!\n")
